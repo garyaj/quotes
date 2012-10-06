d8e674ae267a47607d20c9d3be0295887820866c
Gary Ashton-Jones <gary@ashton-jones.com.au>
2012-10-06 14:28:34 +1000

    Added getidxdaily_dji.pl to retrieve US index quotes

---
+++ bin/getidxdaily_dji.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE:  testscrappy.pl
+#
+#        USAGE:  ./testscrappy.pl  
+#
+#  DESCRIPTION:  Test Scrappy on Yahoo Finance pages.
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Gary Ashton-Jones (GAJ), gary@ashton-jones.com.au
+#      COMPANY:  Ashton-Jones Consulting
+#      VERSION:  1.0
+#      CREATED:  09/03/2011 22:44:03
+#     REVISION:  24/05/2012 17:23:00 GAJ Updated Scrappy.
+#===============================================================================
+
+use 5.010;
+
+use Scrappy;
+use Date::Parse;
+my $year = (localtime(time))[5] + 1900;
+my $yearstring = " $year 00:00:01";
+#printf "%02d/%02d/%04d\n", $day, $month+1, $year+1900;
+my ($open, $date, $high, $low, $close);
+my $url;
+for my $code (qw(DJI IXIC GSPC)) {
+  $url = "http://finance.yahoo.com/q/bc?s=%5E$code+Basic+Chart&t=1d";
+  ($open, $high, $low, $close) = (0,0,0,0);
+  my $scraper = Scrappy->new;
+  if ($scraper->get($url)->page_loaded) {
+    $scraper->select('span.time_rtq_ticker span')->each(
+      sub {
+        ($close = shift->{text}) =~ s/,//g;
+      }
+    );
+    $scraper->select('span.time_rtq span span')->each(
+      sub {
+        my $daymon = shift->{text};
+        my ($ss,$mm,$hh,$day,$month,$year) = strptime("$daymon$yearstring");
+        $date = sprintf "%4d%02d%02d", $year+1900, $month+1, $day;
+      }
+    );
+    $scraper->select('table#table1 tr td.yfnc_tabledata1')->each(
+      sub {
+        my $tag = shift;
+        $open = $tag->{text};
+        if ($open =~ /N\/A/) {
+          $open = 0;
+        } else {
+          $open =~ s/,//g;
+        }
+      }
+    );
+    $scraper->select('table#table2 tr td.yfnc_tabledata1 span span')->each(
+      sub {
+        my $tag = shift;
+        if ($tag->{id} =~ /^yfs_g00_/) {
+          $low = $tag->{text};
+          if ($low =~ /N\/A/) {
+            $low = 0;
+          } else {
+            $low =~ s/,//g;
+          }
+        } elsif ($tag->{id} =~ /^yfs_h00_/) {
+          $high = $tag->{text};
+          if ($high =~ /N\/A/) {
+            $high = 0;
+          } else {
+            $high =~ s/,//g;
+          }
+        }
+      }
+    );
+  }
+  printf "%s,%s,%.3f,%.3f,%.3f,%.3f,0\n",
+    $code, $date, $open, $high, $low, $close;
+}
+
+# vi:ai:et:sw=2 ts=2
+
